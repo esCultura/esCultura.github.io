8da4773dc248e21da9844c9a1ae99a826b11e49f
angelPrat <angel.prat@estudiantat.upc.edu>
2023-05-05 08:27:31 +0200

    Task #214, 216

--- webAdmin/src/views/SingUpOrg.vue
+++ webAdmin/src/views/SingUpOrg.vue
@@ -1,46 +1,72 @@
 <template>
-
-  <div>
-
-      <v-btn @click="fetchUser()">A</v-btn>
-
-      <div> registrar org </div>
-  </div>
+    <div class="background">
+        <v-card
+        height="600"
+        width="500"
+        style="border-radius: 40px;"
+        >
+        <v-card-title class="text-center mt-3">Sing Up</v-card-title>
+        <v-card-text>
+            <v-text-field v-model="username" label="Username" required style="margin-top: 50px;"></v-text-field>
+            <v-text-field v-model="email" label="Email" required style="margin-top: 20px;"></v-text-field>
+            <v-text-field v-model="password" label="Password" type="password" required style="margin-top: 20px;"></v-text-field>
+            <v-text-field @keydown.enter="doSingUp()" v-model="password2" label="Password2" type="password" required style="margin-top: 20px;"></v-text-field>
+            <v-btn @click="doSingUp()" color="#2eca5a" class="mx-auto d-block mt-8" >Sing Up</v-btn>
+        </v-card-text>
+        </v-card>
+    </div>
 
 </template>
 
 <script>
-  import {simpleFetch} from '../utils/utilFunctions';
+  import {ref} from 'vue';
 
   export default {
       
       name: "RegistrarOrg",
-      /*
-      Si es necesari l'estructura es aixi
-      components: {
-          exempleComponent
-      },
-      props: {
-          title: String
-      },
-      setup(props) {
-      */
       setup() {
 
-          
-          async function fetchUser() {
-              let issues;
-              simpleFetch("issues/", "GET", "").then((data) => issues = data);    
-              console.log("issues: ", issues);
-          }
+            let email = ref("");
+            let username = ref("");
+            let password = ref("");
+            let password2 = ref("");
+
+            let host = 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/';
+
+
+            async function doSingUp() {
+                console.log("do SingUp");
+                let response = await fetch(host+"usuaris/sing_up/organitzadors",  {   
+                    method: "POST",
+                    headers: {
+                        'Accept': 'application/json',
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({email:  email.value, username: username.value, password: password.value, password2: password2}),
+                })
+                console.log("response: ", response);
+                let dataD = await response.json();
+                console.log("sing up org data: ", dataD);
+            }
 
           return {
-              fetchUser
+              email,
+              username,
+              password,
+              password2,
+              doSingUp
           }
       }
   }
 </script>
 
 <style scoped>
+    .background {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(to left top, #2eca5a, #00FFFF);
+    }
 
 </style>
