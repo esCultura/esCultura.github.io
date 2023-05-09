20757d97c5473e4f083905c141d51f69d30786e3
angelPrat <angel.prat@estudiantat.upc.edu>
2023-05-09 19:59:53 +0200

    Task #212, 214

--- webAdmin/src/views/Login.vue
+++ webAdmin/src/views/Login.vue
@@ -1,19 +1,31 @@
 <template>
 
   <div class="background">
-    <v-card
-      height="600"
-      width="500"
-      style="border-radius: 40px;"
-    >
-      <v-card-title class="text-center mt-3">LoginP</v-card-title>
-      <v-card-text>
-        <v-text-field v-model="username" label="Username" required style="margin-top: 100px;"></v-text-field>
-        <v-text-field @keydown.enter="doLogin()" v-model="password" label="Password" type="password" required style="margin-top: 30px;"></v-text-field>
-        <v-switch v-model="admin" label="Admin" color="success"></v-switch>
-        <v-btn @click="doLogin()" color="#2eca5a" class="mx-auto d-block mt-16" >Login</v-btn>
-      </v-card-text>
-    </v-card>
+    <div>
+      <v-alert
+        v-show="msgErrorLogin != ''"
+        class="mx-3 my-3"
+        density="compact"
+        type="error"
+        title="Error login"
+        :text= msgErrorLogin
+      ></v-alert>
+    </div>
+    <div class="fomrStyle">
+      <v-card
+        height="600"
+        width="500"
+        style="border-radius: 40px;"
+      >
+        <v-card-title class="text-center mt-3">Login</v-card-title>
+        <v-card-text>
+          <v-text-field v-model="username" label="Username" required style="margin-top: 100px;"></v-text-field>
+          <v-text-field @keydown.enter="doLogin()" v-model="password" label="Password" type="password" required style="margin-top: 30px;"></v-text-field>
+          <v-switch v-model="admin" label="Admin" color="success"></v-switch>
+          <v-btn @click="doLogin()" color="#2eca5a" class="mx-auto d-block mt-16" >Login</v-btn>
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 
 </template>
@@ -35,12 +47,16 @@
         let password = ref('');
         let admin = ref(false);
 
+        let msgErrorLogin = ref('');
+
         async function doLogin() {
           let data;
           let host = 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/';
 
           if (admin.value) {
-            let response = await fetch(host+"usuaris/login/admins",  {   
+            msgErrorLogin.value = '';
+
+            let response = await fetch(host+"usuaris/login/admins/",  {   
               method: "POST",
               headers: {
                   'Accept': 'application/json',
@@ -48,16 +64,25 @@
               },
               body: JSON.stringify({username: username.value, password: password.value}),
             })
-            console.log("response: ", response);
-            let dataD = await response.json();
-            console.log("login admin data: ", dataD);
-            setToken(dataD.token);
-
-            //redirect path --> /Review
-            router.push('/Review');
+            let data = await response.json();
+            console.log("data: ", data);
+            if (data.non_field_errors) {
+              let str = '';
+              for (let string of data.non_field_errors) {
+                str += string + '\n';
+              }
+              msgErrorLogin.value = str;
+            }
+            else if (data.token) {
+              setToken(data.token);
+              //redirect path --> /Review
+              router.push('/Review');
+            }
           }
           else {
-            let response =  await fetch(host+"usuaris/login/organitzadors",  {   
+            msgErrorLogin.value = '';
+
+            let response =  await fetch(host+"usuaris/login/organitzadors/",  {   
               method: "POST",
               headers: {
                   'Accept': 'application/json',
@@ -66,22 +91,29 @@
               body: JSON.stringify({username: username.value, password: password.value}),
             })
             console.log("response: ", response);
-            let dataD;
-            dataD = await response.json();
-            console.log("login org data: ", dataD);
-            setToken(dataD.token);
-
-            //redirect path  --> /edit
-            router.push('/edit');
+            let data = await response.json();
+            console.log("login org data: ", data);
+            if (data.non_field_errors) {
+              let str = '';
+              for (let string of data.non_field_errors) {
+                str += string + '\n';
+              }
+              msgErrorLogin.value = str;
+            }
+            else if (data.token) {
+              setToken(data.token);
+              //redirect path  --> /edit
+              router.push('/edit');
+            }
           }
 
         }
-
 
         return {
           username,
           password,
           admin,
+          msgErrorLogin,
           doLogin
         }
       }
@@ -93,9 +125,15 @@
   .background {
     height: 100vh;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
     background: linear-gradient(to left top, #2eca5a, #00FFFF);
   }
+  .fomrStyle {
+        display: flex; 
+        justify-content: center; 
+        margin-top: auto; 
+        margin-bottom: auto;
+    }
 
 </style>
